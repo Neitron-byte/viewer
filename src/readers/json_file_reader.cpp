5222ea590bf0adfe4f415e407fc4347a57368ef8
Nick <yakimov_87@mail.ru>
2025-05-10 23:32:41 +0300

    fix: add json parse error

--- src/readers/json_file_reader.cpp
+++ src/readers/json_file_reader.cpp
@@ -3,6 +3,7 @@
 #include <QFile>
 #include <QJsonObject>
 #include <QJsonDocument>
+#include <QJsonParseError>
 
 #include "definitions.h"
 
@@ -15,14 +16,19 @@ bool View::JsonFileReader::read(const QString &file_path, Record &record)
         return false;
     }
     QByteArray array = file.readAll();
-    QJsonObject root = QJsonDocument::fromJson(array).object();
+    QJsonParseError parse_error{QJsonParseError::NoError};
+    QJsonObject root = QJsonDocument::fromJson(array,&parse_error).object();
 
-    if(!root.contains(rootTag))
+    if(parse_error.error != QJsonParseError::NoError)
+    {
+        _error = QObject::tr("Ошибка парсинга Json: %1. Позиция: %2").arg(parse_error.errorString()).arg(parse_error.offset);
+        return false;
+    }
+    if(root.contains(rootTag))
     {
         _error = QObject::tr("Формат файла не поддерживается");
         return false;
     }
-
     record.fromJson(root.value(rootTag).toObject());
     file.close();
     return true;
